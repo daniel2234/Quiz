dba2a01d190f40c4283e2d06de262b38febe39a6
Daniel Kwiatkowski <kwiatkowski.daniel@gmail.com>
2016-04-08 16:38:13 -0400

    Linked xml to objects

---
+++ app/src/main/java/programs/quiz/Question.java
@@ -0,0 +1,31 @@
+package programs.quiz;
+
+/**
+ * Created by Daniel on 2016-04-08.
+ */
+public class Question {
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue){
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+}
